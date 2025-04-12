80dcaaf50fd782aae1eab25cf954b806d591d7ce
Himanshi Singh <97846576+HimanshiSingh2304@users.noreply.github.com>
2025-04-12 12:20:41 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ import router from "./Routes/index.js"
 dotenv.config();
 const app= express()
 app.use(cors({
-    origin: "https://mern-sign-up-log-in.vercel.app/",
+    origin: "https://mern-sign-up-log-in.vercel.app",
     credentials:true
 }))
 app.use(express.json())
